260489a0d54c0d7b188f21b82474e603cf139daa
Ignis Codex <90404350+IgnisCodex@users.noreply.github.com>
2026-01-22 00:54:18 +0000

    Updated premake5.lua.

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,8 @@ project "GLFW"
 	kind "StaticLib"
 	language "C"
 	staticruntime "on"
-
-	targetdir ("%{wks.location}/build/bin/" .. OUTPUT_DIR .. "/%{prj.name}")
-	objdir ("%{wks.location}/build/int/" .. OUTPUT_DIR .. "/%{prj.name}")
+	targetdir (BIN_DIR .. "/%{prj.name}")
+	objdir (OBJ_DIR .. "/%{prj.name}")
 
 	files {
 		"include/GLFW/glfw3.h",
@@ -24,50 +23,6 @@ project "GLFW"
 		"src/vulkan.c",
 		"src/window.c",
 	}
-
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files {
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines {
-			"_GLFW_X11"
-		}
-
-	filter "system:macosx"
-		pic "On"
-
-		files {
-			"src/cocoa_init.m",
-			"src/cocoa_monitor.m",
-			"src/cocoa_window.m",
-			"src/cocoa_joystick.m",
-			"src/cocoa_time.c",
-			"src/nsgl_context.m",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/osmesa_context.c",
-			"src/egl_context.c"
-		}
-
-		defines {
-			"_GLFW_COCOA"
-		}
 
 	filter "system:windows"
 		systemversion "latest"
@@ -104,7 +59,7 @@ project "GLFW"
 		runtime "Release"
 		optimize "speed"
 
-    filter "configurations:Dist"
+    filter "configurations:Distribution"
 		runtime "Release"
 		optimize "speed"
         symbols "off"
